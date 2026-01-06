01fa615714a20d1ea1dc870654c5b88b01db01ed
lariod12 <lariodnguyen12@gmail.com>
2026-01-06 20:54:22 +0700

    Enhance game interface by adding tutorial button and updating styles for action buttons

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 // Main entry point for Betrayal at House on the Hill
 
 const playButton = document.getElementById('playButton');
+const tutorialButton = document.getElementById('tutorialButton');
 
-playButton.addEventListener('click', () => {
+playButton?.addEventListener('click', () => {
     console.log('Play button clicked!');
     // TODO: Navigate to game screen
     alert('Game starting soon...');
+});
+
+tutorialButton?.addEventListener('click', () => {
+    console.log('Tutorial button clicked!');
+    // TODO: Navigate to tutorial screen
+    alert('Tutorial coming soon...');
 });
 
 // Initialize app
